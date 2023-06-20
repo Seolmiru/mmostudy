0c16af4031cac52197ad5647a3b7a110230d6b3a
Seolmin <drhon10@gmail.com>
2023-06-21 07:34:28 +0900

    test

---
+++ .vs/TestProgram/TestProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            int maxHp;
+
+            //Todo
+            maxHp = hp;
+
+            Console.WriteLine(hp);
+        }
+    }
+}
--- .vs/testRPG/testRPG/Program.cs
+++ .vs/testRPG/testRPG/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp;
+
+            hp = 100;
+
+            Console.WriteLine(hp);
+        }
+    }
+}
